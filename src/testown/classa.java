c35074e692584bab9fbaa114c323fdbacd730bc5
chetan <ohol.chetan@gmail.com>
2020-02-06 16:41:34 +0530

    adding file to repo

---
+++ src/testown/classa.java
@@ -0,0 +1,23 @@
+package testown;
+
+public class classa {
+	
+
+   classa a = new classa();
+    
+ public static void add(int a, int b)
+
+{
+	int c;
+	
+	c=a+b;
+	
+	
+}
+
+}
+
+
+
+
+
---
+++ src/testown/classb.java
@@ -0,0 +1,33 @@
+package testown;
+
+public class classb{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				 	 
+		  second sec= new second();
+		 		  
+		  sec.add(10, 19);
+		
+
+	}
+
+}
+
+class second
+{
+
+	public void add(int a, int b)
+
+	{
+		
+		int c;
+		
+		c= a+b;
+		
+		System.out.println(c);
+		
+		
+	}
+	
+}
